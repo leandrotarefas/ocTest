4f3de0f7fd90e342f281289f6ba60ee3785e8697
LEANDRO M MELO <30564192+leandrotarefas@users.noreply.github.com>
2023-02-27 00:59:10 -0300

    Add files via upload

---
+++ ocChild/app.js
@@ -0,0 +1,93 @@
+const { exec } = require('child_process');
+const util = require('util');
+const execPromise = util.promisify(exec);
+
+const tryCatch = function tryCatch(promise) {
+    return promise.then((data) => {
+        return [null, data];
+    }).catch((err) => [err]);
+};
+
+const login = async () => {
+    const username = "chumbado?";
+    const password = "chumbado?";
+    const ocLoginCommand = `oc login --insecure-skip-tls-verify=true -u ${username} -p ${password}`;
+
+    console.log("Fazendo login via OC...");
+    const ocLogin = await tryCatch(execPromise(ocLoginCommand));
+    return ocLogin;
+}
+
+const kill = async (process, item) => {
+    const appName = process;
+    const ocRemoveCommand = `oc delete ${item} ${appName}`;
+
+    console.log(`Removendo ${item}`);
+    const ocRemove = await tryCatch(execPromise(ocRemoveCommand));
+    return ocRemove;
+}
+
+const stop = (msg) => {
+    console.log(msg);
+    setTimeout(() => {
+        console.log("Servico encerrado!");
+        process.exit(0);
+    }, 15000);
+}
+
+const start = async () => {
+
+    //login
+    const [ocLoginError, ocLogin] = login();
+
+    //erro no login, loga e sai da aplicacao
+    if (ocLoginError) {
+        //finaliza a aplicacao
+        stop(ocLoginError);
+        return;
+    }
+
+    //login bem sucedido
+    if (ocLogin) {
+
+        console.log("Faz alguma coisa...");
+
+        //aguarda 5 segundos
+        setTimeout(() => {
+
+            console.log("Encerrando o Pod...");
+
+            //lista de itens para encerrar
+            const items = ["deployment", "service", "deployments.apps", "buildconfigs.build.openshift.io", "route"];
+
+            let statusOk = true;
+
+            //encerrar um a um
+            for (let item of items) {
+
+                const [ocRemovedError, ocRemoved] = kill("processChumbado?", item);
+                if (ocRemovedError) {
+                    statusOk = false;
+                    //finaliza a aplicacao
+                    stop(ocRemovedError);
+                    return;
+                }
+
+                console.log(`${item} removido!`)
+                console.log(ocRemoved);
+            }
+
+            //tudo certo finaliza a aplicacao
+            if (statusOk) {
+                stop("Processamento conluido com sucesso!");
+            }
+
+        }, 5000);
+    }
+
+
+}
+
+console.log("Pod criado!");
+
+start();
